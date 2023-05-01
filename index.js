6af449e9fc948616e8f53ba467c4326610932863
Gdel-J <jessygdelvecchio@gmail.com>
2023-05-01 13:03:35 -0400

    fixing file issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Importing classes from ./lib/shapes directory
-const { Triangle, Square, Circle } = require("./lib/shapes ");
+const { Triangle, Square, Circle } = require;
 
 // Function writes the SVG file using user data from inquirer prompts
 function writeToFile(fileName, answers) {
